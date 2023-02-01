9b753d30668884a5dfd47c951e0960cb51b1157d
Yammi08 <Yammi081@hotmail.com>
2023-02-01 11:01:06 -0500

    proyectos añadidos

--- js/projects.js
+++ js/projects.js
@@ -1,14 +1,41 @@
 var projects = ()=>
 {
     const projects = [
+        {
+            title:'Encriptador',
+            image:'./img/projects/alura-encriptador.PNG',
+            imageAlt:'encriptador',
+            imageHerf:'https://yammi08.github.io/Alura-Challenge/'
+        },
+        {
+            title:'Ahorcado',
+            image:'./img/projects/alura-ahorcado.PNG',
+            imageAlt:'ahorcado',
+            imageHerf:'https://yammi08.github.io/Ahorcado/'
+        },
+        {
+            title:'Slime run',
+            image:'./img/projects/run-slime.png',
+            imageAlt:'slime',
+            imageHerf:'https://github.com/Yammi08/SlimeRun'
+        },
+        {
+            title:'Generador de habitaciones',
+            image:'./img/projects/generate-rooms.png',
+            imageAlt:'generate',
+            imageHerf:'https://github.com/Yammi08/RandomGenerate'
+        }
+
     ];
     const project = ({title,imageHerf,image,imageAlt})=>
     {
         console.log(title);
             return`
             <div class="container__project">
-            <h3 class="title__project">${title}</h3>
             <a href= ${imageHerf} class="link__project">
+                <div class="container__title">
+                    <h3 class="title__project">${title}</h3>
+                </div>
                 <img src= ${image} alt=${imageAlt} class="image__project">
             </a>
             </div>`;
